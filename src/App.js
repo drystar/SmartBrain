1c1f0d43f8f4e37fae90bc0e9a5d4b5a29cc2f1e
drystar <chris.drysdale12@gmail.com>
2020-02-22 11:17:24 -0800

    fix bug

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Navigation from "./Components/Navigation/Navigation";
 import "./App.css";
 
-class App extends Component() {
+class App extends Component {
   render() {
     return (
       <div className="App">
