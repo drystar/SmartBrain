0400c5adfa5804b4f0ccb1f913cd0d3229e5c026
drystar <chris.drysdale12@gmail.com>
2020-02-24 14:49:53 -0800

    update input with state

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
   }
 
   onInputChange = event => {
-    console.log(event.target.value);
+    this.setState({ input: event.target.value });
   };
 
   onButtonSubmit = () => {
