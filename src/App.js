d9422ff35513b2d3c4ad85592beab68df84efb39
drystar <chris.drysdale12@gmail.com>
2020-02-25 11:41:36 -0800

    create function to calculate bounding/ face box

--- src/App.js
+++ src/App.js
@@ -43,6 +43,17 @@ class App extends Component {
     const width = Number(image.width);
     const height = Number(image.height);
     console.log(width, height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  displayFaceBox = box => {
+    console.log(box);
+    this.setState({ box: box });
   };
 
   onInputChange = event => {
@@ -54,8 +65,9 @@ class App extends Component {
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
       .then(response =>
-        this.calculateFacelocation(response).catch(err => console.log(err))
-      );
+        this.displayFaceBox(this.calculateFacelocation(response))
+      )
+      .catch(err => console.log(err));
   };
 
   // COLOR_MODEL: 'eeed0b6733a644cea07cf4c60f87ebb7',
