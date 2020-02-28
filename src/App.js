4195a94f36a65082f8e786eee856ddbbc332fb98
drystar <chris.drysdale12@gmail.com>
2020-02-28 12:02:22 -0800

    fix nav display | add conditionals utilising new isSignedIn state

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
       imageUrl: "",
       box: {},
       route: "Signin",
-      isSignedIn: "false"
+      isSignedIn: false
     };
   }
 
@@ -87,7 +87,10 @@ class App extends Component {
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation onRouteChange={this.onRouteChange} />
+        <Navigation
+          isSignedIn={this.state.isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
         {this.state.route === "home" ? (
           <div>
             <Logo />
