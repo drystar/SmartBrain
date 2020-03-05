abd49939d9cafb49ef08f4b59364a3b81c04ac28
drystar <chris.drysdale12@gmail.com>
2020-03-05 10:02:07 -0800

    plan API & endpoints

--- backEnd/server.js
+++ backEnd/server.js
@@ -9,3 +9,12 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("App is Running on Port 3000 - Looking Good!");
 });
+
+/*
+/ -- res = this is working
+/ signin --> POST success/ fail
+/ register --> POST = user
+/ profile/:userId --> GET = user
+/ image --> PUT --> user
+
+*/
