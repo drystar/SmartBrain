5e7f6a04e1774d1fdbceaa468ac229245faf9112
drystar <chris.drysdale12@gmail.com>
2020-03-05 09:52:26 -0800

    create basic route | test witj postman

--- backEnd/server.js
+++ backEnd/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("This is Working");
+});
+
 app.listen(3000, () => {
   console.log("App is Running on Port 3000 - Looking Good!");
 });
