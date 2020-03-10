5e8f20bc3889baab9ddb5afbd8fccf92da593a21
drystar <chris.drysdale12@gmail.com>
2020-03-09 23:08:09 -0700

    misc update

--- backEnd/server.js
+++ backEnd/server.js
@@ -5,9 +5,9 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 // app.use(express.json());
-app.use(cors());
 
 // hard codeed database | will be dynamic later
 const database = {
