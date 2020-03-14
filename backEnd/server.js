82f254f1e6152488196b9b3ca0a311b0460e57b1
drystar <chris.drysdale12@gmail.com>
2020-03-14 13:34:37 -0700

    create isValid function to check password with bcrypt

--- backEnd/server.js
+++ backEnd/server.js
@@ -68,8 +68,21 @@ app.post("/signin", (req, res) => {
     .from("login")
     .where("email", "=", req.body.email)
     .then(data => {
-      console.log(data);
-    });
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json("Unable to get User"));
+      } else {
+        res.status(400).json("Invalid Credentials");
+      }
+    })
+    .catch(err => res.status(400).json("Invalid Credentials"));
 });
 
 // register route
