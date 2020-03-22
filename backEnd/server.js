dae6860ee4fa0084fb86e02dbf64f74853276173
drystar <chris.drysdale12@gmail.com>
2020-03-22 14:44:29 -0700

    create a function for  register endpoint

--- backEnd/server.js
+++ backEnd/server.js
@@ -49,33 +49,7 @@ app.post("/signin", (req, res) => {
 });
 
 // register route
-app.post("/register", (req, res) => {
-  const { email, name, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  db.transaction(trx => {
-    trx
-      .insert({
-        hash: hash,
-        email: email
-      })
-      .into("login")
-      .returning("email")
-      .then(loginEmail => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0]);
-          });
-      })
-      .then(trx.comit)
-      .catch(trx.rollback);
-  }).catch(err => res.status(400).json("Unable to Join"));
-});
+app.post("/register", register.handleRegister);
 
 // user id/ profile request route
 app.get("/profile/:id", (req, res) => {
