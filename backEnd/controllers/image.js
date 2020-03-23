d6d368da6858b88a93626a9f719cadf89afd61e5
drystar <chris.drysdale12@gmail.com>
2020-03-23 15:08:34 -0700

    add respone and error handling to handleAPI function

--- backEnd/controllers/image.js
+++ backEnd/controllers/image.js
@@ -5,7 +5,12 @@ const app = new Clarifai.App({
 });
 
 const handleApiCall = (req, res) => {
-  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input);
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json("API request failed"));
 };
 
 const handleImage = db => (req, res) => {
