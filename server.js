96cdaf57e70e6424db1a44199649fed81d28238a
drystar <chris.drysdale12@gmail.com>
2020-03-25 00:12:58 -0700

    remove database  from route

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  // res.send("This is Working");
-  res.send(database.users);
+  res.send("This is Working");
+  // res.send(database.users);
 });
 
 // signin route
