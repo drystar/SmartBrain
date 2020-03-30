a0ca6fcbcab86126bf8bfb4df942df2a8ff24168
drystar <chris.drysdale12@gmail.com>
2020-03-29 20:09:17 -0700

    update database to heroku host'
    \q
    
    '
    '

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-metric-42927",
     user: "chris",
     password: "",
     database: "smart-brain"
